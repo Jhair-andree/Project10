05c0366b86c855e7b44a0fa04520f1210c815c81
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2021-11-25 22:01:20 -0500

    Create conferenceroomActivity.kt

---
+++ conferenceroomActivity.kt
@@ -0,0 +1,40 @@
+package com.example.colegioscreser
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.TextView
+import com.example.colegioscreser.databinding.ActivityFullscreenBinding
+
+class MainActivity: AppCompatActivity (){
+    override fun onCreate (savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.android.main_activity)
+        override onDraw()
+        {
+            echo$="Please entry the choice prefered: (1) Low (2) Medium (3) High (4) Higher (5) Much higher"
+        }
+    }
+private class TactilEntry tactilentry;
+private class OnTouch ontouch;
+private class Receipt receipt=new private array[1,100];
+    if(Receipt receipt == % 2)
+    {
+        for(Receipt receipt=0;Receipt receipt < TactilEntry tactilentry; Receipt receipt++)
+        {
+            override setOnClickListener("Search bar\n")
+        }
+        for(TactilEntry tactilentry=0;TactilEntry tactilentry<OnTouch ontouch;TactilEntry tactilentry++)
+        {
+            override setOnClickListener("0=\n")
+        }
+    }
+}
+class AuxiliarActivity: AppCompatActivity(){
+    override fun onDraw(savedInstanceState: Bundle?){
+        super.onDraw()
+    }
+}
+
