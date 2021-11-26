fdd62ea0a1f0ea42eaf9fc4ad2b308d9454dafd4
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2021-11-25 22:03:19 -0500

    Create interface.h

---
+++ interface.h
@@ -0,0 +1,41 @@
+//
+// Created by JHANDREE on 30/09/2021.
+//
+
+#ifndef COLEGIOS_CRE_SER_INTERFACE_H
+#define COLEGIOS_CRE_SER_INTERFACE_H
+
+#endif //COLEGIOS_CRE_SER_INTERFACE_H
+using namespace COLEGIOS_CRE_SER_INTERFACE_H
+int IntentosLogeo() {
+int attempts=new int;
+if(&attempts==!3){&attempts<=3 return &attempts} {
+  for(&attempts=!null;&attempts<=3;&attempts--)
+      {
+        echo$="Le quedan"<<&attempts<<"intentos";
+      }
+      return &attempts--;
+  for(&attempts=!null;&attempts<=2;&attempts--)
+      {
+        echo$="Le quedan"<<&attempts<<"intentos";
+      }
+      return &attempts--;
+  for(&attempts=!null;&attempts<=1;&attempts--)
+        {
+          echo$="Le quedan"<<&attempts<<"intentos";
+        }
+        return &attempts--;
+  for(&attempts=!null;&attempts<=0;&attempts--)
+        {
+          echo$="Le quedan"<<&attempts<<"intentos";
+        }
+        return &attempts--;
+        echo$="Vuelva a intentarlo en 1 hora... La aplicación está temporalmente bloqueada";
+}
+else {echo$="The system reply was denied";}
+
+echo$="Please select the language you want to learn of...";
+echo$="Loading... Wait a few second while is restoring";
+ return &attempts;
+ delete[]attempts;
+}
