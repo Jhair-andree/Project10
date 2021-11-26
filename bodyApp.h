fc00e8c1d75ff29fb5a5279aedb7ad23d2102518
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2021-11-25 22:11:25 -0500

    Create bodyApp.h

---
+++ bodyApp.h
@@ -0,0 +1,7 @@
+#pragma once
+using namespace private COLEGIOS_CRE_SER_INTERFACE_H;
+using namespace System;
+class bodyApp: AppCompatActivity(){
+  protected void bodyHTML(StringBuilder bodyApp)
+
+}
