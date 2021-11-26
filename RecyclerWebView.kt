1ab3540b745e05662d1964d6e3bb6c4ff91f2e7b
Jhair-andree <66096069+Jhair-andree@users.noreply.github.com>
2021-11-25 22:10:32 -0500

    Create RecyclerWebView.kt

---
+++ RecyclerWebView.kt
@@ -0,0 +1,24 @@
+package com.example.colegioscreser
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+import android.widget.TextView
+import com.example.colegioscreser.databinding.ActivityFullscreenBinding
+
+class MainActivity: AppCompatActivity (){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        private val int tareas = ArrayList <Tareas> ()
+        tareas.add(Tarea(nombre: "ACTIVIDADES DE CLASE", terminada true))
+        tareas.add(Tarea(nombre: "Recursos de aprendizaje", terminada false))
+
+        recycler_view.adapter = TareaAdapter(tareas)
+        recycler_view.layoutManager = LinearLayoutManager(context: this)
+        recycler_view.addItemDecoration(DividerItemDecoration (context: this, LinearLayoutManager.VERTICAL)
+    }
+}
